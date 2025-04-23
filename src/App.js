9f6f799def0653da2763837565987d63138bcc60
blised <redtwo9797@gmail.com>
2025-04-23 12:52:56 -0600

    RenderNavigation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import styled from "styled-components";
 import bg from './img/bg.png'
-import { MainLayout } from "./styles/Layouts";
+import {MainLayout} from "./styles/Layouts";
+import Orb from "./Components/Orb/Orb";
+import Navigation from "./Components/Navigation/Navigation";
 
 function App() {
 
   return(
     <AppStyled bg={bg} className="App">
+      <Orb />
       <MainLayout>
-        <h1>
-          hello
-        </h1>
+        <Navigation/>
       </MainLayout>
     </AppStyled>
   );
