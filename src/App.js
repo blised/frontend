f9db0189a05677e32af6a8d99325d39770735374
blised <redtwo9797@gmail.com>
2025-04-23 14:25:36 -0600

    mainhello

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useMemo, useState} from "react";
 import styled from "styled-components";
 import bg from './img/bg.png'
 import {MainLayout} from "./styles/Layouts";
@@ -6,11 +7,22 @@ import Navigation from "./Components/Navigation/Navigation";
 
 function App() {
 
+  const [active, setActive] = React.useState(1)
+
+  const orbMemo = useMemo(() => {
+    return <Orb />
+  }, [])
+
   return(
     <AppStyled bg={bg} className="App">
-      <Orb />
+      {orbMemo}
       <MainLayout>
-        <Navigation/>
+        <Navigation active={active} setActive={setActive} />
+        <main>
+          <h1>
+            hello
+          </h1>
+        </main>
       </MainLayout>
     </AppStyled>
   );
@@ -20,6 +32,16 @@ const AppStyled = styled.div`
   height: 100vh;
   background-image: url(${props => props.bg});
   position: relative;
+  main{
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #FFFFFF;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar{
+      width: 0;
+  }
 `;
 
 
