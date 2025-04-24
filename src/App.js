73c4dce6fe42fe9fa3268d778da5a434b09866ee
blised <redtwo9797@gmail.com>
2025-04-23 19:54:08 -0600

    .form!=.div

--- src/App.js
+++ src/App.js
@@ -4,10 +4,32 @@ import bg from './img/bg.png'
 import {MainLayout} from "./styles/Layouts";
 import Orb from "./Components/Orb/Orb";
 import Navigation from "./Components/Navigation/Navigation";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Income from './Components/Income/Income';
+import Expenses from './Components/Expenses/Expenses';
+import { useGlobalContext } from "./context/globalContext";
 
 function App() {
 
-  const [active, setActive] = React.useState(1)
+  const [active, setActive] = useState(1)
+
+  const global = useGlobalContext()
+  console.log(global);
+
+  const displayData = () => {
+    switch(active){
+      case 1:
+        return <Dashboard />
+      case 2:
+        return <Dashboard />
+      case 3:
+        return <Income />
+      case 4:
+        return <Expenses />
+      default:
+        return <Dashboard />
+    }
+  }
 
   const orbMemo = useMemo(() => {
     return <Orb />
@@ -19,9 +41,7 @@ function App() {
       <MainLayout>
         <Navigation active={active} setActive={setActive} />
         <main>
-          <h1>
-            hello
-          </h1>
+          {displayData()}
         </main>
       </MainLayout>
     </AppStyled>
